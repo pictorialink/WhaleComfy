35761b2880ef86c475f135145923f1c1ee29a11a
wlx503190755 <62003217+wlx503190755@users.noreply.github.com>
2025-06-10 18:15:54 +0800

    Update entrypoint.sh

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 #
 ## start ComfyUI Server
+if [ -f "custom_nodes/ComfyUI-Custom-Node-Config/files/config.sh" ]; then
+    sh custom_nodes/ComfyUI-Custom-Node-Config/files/config.sh
+fi 
 python3 main.py --listen
