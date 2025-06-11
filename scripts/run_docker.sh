0b0c3837cf6d9089fcdf106b09210725b5ed03d5
wanglixin <wanglixin@huanqiu.com>
2025-06-11 14:30:12 +0800

    合并问题处理

--- scripts/run_docker.sh
+++ scripts/run_docker.sh
@@ -94,7 +94,7 @@ init_system() {
 
 
 get_image_name() {
-    IMAGE_NAME=$(grep 'image:'|grep -v 'comfyui' "$PROJECT_ROOT/docker/docker-compose.yml" | awk '{print $2}')
+    IMAGE_NAME=$(grep -A 1 "comfyui:" docker/docker-compose.yml | grep "image:" | awk '{print $2}')
     IMAGE_NAME=$(echo "$IMAGE_NAME" | tr -d '\r')
     echo "$IMAGE_NAME"
 }
