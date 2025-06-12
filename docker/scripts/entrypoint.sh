8ae3bc56fc56d062662cb352da953e5fb72e905f
wlx503190755 <503190755@qq.com>
2025-06-12 17:13:53 +0800

    mac dockerfile and 启动脚本

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -4,4 +4,4 @@
 if [ -f "custom_nodes/ComfyUI-Custom-Node-Config/files/config.sh" ]; then
     sh custom_nodes/ComfyUI-Custom-Node-Config/files/config.sh
 fi 
-python3 main.py --listen
+/ComfyUI/venv/bin/python main.py --listen
