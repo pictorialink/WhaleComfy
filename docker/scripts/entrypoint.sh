6ed68c19c951a3d553c7e703fa8fb7e342aad8ad
wanglixin <wanglixin@huanqiu.com>
2025-07-21 11:01:55 +0800

    node 检查

--- docker/scripts/entrypoint.sh
+++ docker/scripts/entrypoint.sh
@@ -4,4 +4,6 @@
 if [ -f "custom_nodes/ComfyUI-Custom-Node-Config/files/config.sh" ]; then
     sh custom_nodes/ComfyUI-Custom-Node-Config/files/config.sh
 fi 
+/ComfyUI/venv/bin/python /app/scripts/get_repo_tags.py
+
 /ComfyUI/venv/bin/python main.py --listen
